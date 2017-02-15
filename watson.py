46ff5eae604c3c42fea7d3e5b0f71a3e50d78d17
mathias3 <maciej.osowski@gmail.com>
2017-02-15 22:45:42 +0000

    Update watson.py

--- watson.py
+++ watson.py
@@ -4,3 +4,9 @@ import requests
 import json
 import twitter
 from watson_developer_cloud import PersonalityInsightsV2 as PersonalityInsights
+
+def analyze(handle):
+	twitter_consumer_key= ''
+	twitter_consumer_secret= ''
+	twitter_access_token= ''
+	twitter_access_token_secret= ''
