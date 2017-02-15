2e688e5695b61ecb3f48b52e966a50d243b25f0a
mathias3 <maciej.osowski@gmail.com>
2017-02-15 22:46:05 +0000

    Update watson.py

--- watson.py
+++ watson.py
@@ -10,3 +10,12 @@ def analyze(handle):
 	twitter_consumer_secret= ''
 	twitter_access_token= ''
 	twitter_access_token_secret= ''
+	
+	twitter_api = twitter.Api(consumer_key=twitter_consumer_key, 			consumer_secret=twitter_consumer_secret, access_token_key=twitter_access_token, access_token_secret=twitter_access_secret)
+
+
+	statuses = twitter_api.GetUserTimeline(screen_name=handle, count=200, include_rts=False)
+	text = ""
+	for status in statuses:
+            if (status.lang =='en'): #English tweets
+                    text += status.text.encode('utf-8')
